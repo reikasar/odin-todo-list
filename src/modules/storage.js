95923212c1348347717c00199f05326ba517a431
reikasar <183247743+reikasar@users.noreply.github.com>
2026-05-02 18:53:16 +0300

    add array for projects and func for adding to the list

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,3 +1,11 @@
 // here goes storage logic
 
-export const greeting4 = "Hello from storage.js!"
+export const greeting4 = "Hello from storage.js!";
+
+// empty array for string projects list
+let projects = [];
+
+// add project to projects array
+export function addProject(project) {
+   projects.push(project)
+};
