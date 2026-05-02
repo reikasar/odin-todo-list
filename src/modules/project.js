0f93dc286ec4fd887f6c4976798ac94561fa2d4d
reikasar <183247743+reikasar@users.noreply.github.com>
2026-05-02 18:04:45 +0300

    add project object

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,3 +1,13 @@
 // here goes project logic
 
-export const greeting = "Hello from project.js!"
+export const greeting = "Hello from project.js!";
+
+// Project object
+export class Project {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.todos = [];
+    this.id = crypto.randomUUID();
+  }
+}
