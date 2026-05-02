96f5610d35a587044a76f685ec155bd981f359fb
reikasar <183247743+reikasar@users.noreply.github.com>
2026-05-02 18:01:31 +0300

    add todo object logic

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,16 @@
 // here goes todo logic
 
-export const greeting2 = "Hello from todo.js!"
+export const greeting2 = "Hello from todo.js!";
+
+// Todo class 
+export class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.id = crypto.randomUUID();
+    }
+}
+
+
