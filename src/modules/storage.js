d3d4aea0e17016cf7e90d77b9bd1e3f0e873a8a3
reikasar <183247743+reikasar@users.noreply.github.com>
2026-05-09 16:48:35 +0300

    add logic for manipulating projects and storiing in in localStorage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -5,7 +5,52 @@ export const greeting4 = "Hello from storage.js!";
 // empty array for string projects list
 let projects = [];
 
-// add project to projects array
+// add project to projects array using push and call saveProject func
 export function addProject(project) {
-   projects.push(project)
+   projects.push(project);
+   saveProject();
 };
+
+// remove project from projects array using filter and call saveProject func
+export function removeProject(id) {
+   let updatedProjects = projects.filter(project => project.id !== id);
+   projects = updatedProjects;
+   saveProject()
+}
+
+
+// gets project form project array using find
+export function getProject(id) {
+   return projects.find(project => project.id === id);  
+}
+
+// return all projects
+export function getProjects() {
+   return projects;
+}
+
+// save project to localStorage
+function saveProject() {
+   localStorage.setItem("Data", JSON.stringify(projects));
+}
+
+// load project from localStorage
+export function loadProject() {
+   let parseableProject = localStorage.getItem("Data");
+   if (parseableProject !== null) {
+      projects = JSON.parse(parseableProject);
+   }
+}
+
+// Storing data
+localStorage.setItem('username', 'JohnDoe');
+
+// Retrieving data
+const user = localStorage.getItem('username');
+console.log(user); // "JohnDoe"
+
+// Removing specific data
+localStorage.removeItem('username');
+
+// Clearing all data
+localStorage.clear();   
